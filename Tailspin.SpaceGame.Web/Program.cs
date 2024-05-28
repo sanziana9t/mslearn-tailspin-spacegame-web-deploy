6ba456c40ad9a63365ea90e930aae80a60618e2d
Sanziana Postavaru <sanziana@9tlabs.com>
2024-05-28 15:48:32 +0200

    Test push

--- Tailspin.SpaceGame.Web/Program.cs
+++ Tailspin.SpaceGame.Web/Program.cs
@@ -8,6 +8,8 @@ namespace TailSpin.SpaceGame.Web
         public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
+
+            
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
